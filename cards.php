1090ea7f50e8d26cb31fd7d7e8872c21cf6a501c
Noel Friedrich <62935655+noel-friedrich@users.noreply.github.com>
2021-03-05 16:06:13 +0100

    Fixed Layering Bug

--- cards.php
+++ cards.php
@@ -13,141 +13,187 @@
 
 <script>
 
-  var size = 0.3
+  let size = 0.3
 
-  var oReq = new XMLHttpRequest()
+  let oReq = new XMLHttpRequest()
   oReq.open("get", "delete-data.php", true)
   oReq.send()
 
-  var canvas = document.getElementById("cardsCanvas")
-  var context = canvas.getContext("2d")
-  var max_layer = 0;
-  canvas.width  = window.innerWidth
-  canvas.height = window.innerHeight
+  let canvas = document.getElementById("cardsCanvas")
+  let context = canvas.getContext("2d")
+  let max_layer = 0;
+  canvas.width  = window.innerWidth - 100
+  canvas.height = window.innerHeight - 100
   images = []
   items = ["clubs", "diamonds", "hearts", "spades"]
   cardnums = ["2","3","4","5","6","7","8","9","10","A","K","J","Q"]
   cards = []
   items.forEach(function(item){
     cardnums.forEach(function(card){
-      var img = new Image()
+      let img = new Image()
       img.src = "https://www.die-quote.de/wp-content/uploads/2021/" + item + "/" + card
       images.push(img)
     })
   })
   desiredPos = []
-  var a_difference = 0;
-  var b_difference = 0;
-  var ab_step = 5;
+  let a_difference = 0;
+  let b_difference = 0;
+  let ab_step = 5;
+  let setId = false;
+  let countc = 0;
   images.forEach(function(image) {
-    var oReq = new XMLHttpRequest()
-    var a = Math.round(1000 / 2 - (image.width * size) + a_difference - (ab_step * (images.length / 2)))
-    var b = Math.round(1000 / 2 - (image.height * size) + b_difference - (ab_step * (images.length / 2)))
+    let oReq = new XMLHttpRequest()
+    let a = Math.round(1000 / 2 - (image.width * size) + a_difference - (ab_step * (images.length / 2)))
+    let b = Math.round(1000 / 2 - (image.height * size) + b_difference - (ab_step * (images.length / 2)))
     a_difference += ab_step
     b_difference += ab_step
-    oReq.open("get", "insert-data.php?x=" + a + "&y=" + b + "&l=" + 0 , true)
+    oReq.open("get", "insert-data.php?x=" + a + "&y=" + b + "&l=" + countc , true)
     oReq.send()
-    cards.push({x:a, y:b, img:image, layer:0})
+    cards.push({x:a, y:b, img:image, layer:0, id:(countc+1)})
     desiredPos.push({x:0, y:0})
+    countc++
   })
   sqlData = []
-  mouseState = {up:true, down:false, dragging:false, draggingId:0}
+  mouseState = {up:true, down:false, dragging:false, draggingId:0, draggingElementId:0}
   relativePos = {x:0, y:0}
-  var requestfinished = false
+  let requestfinished = false
 
-  var tickCount = 0
+  let tickCount = 0
 
   const interval = setInterval(function() {
-    if (mouseState.up) {
+    if (!mouseState.dragging) {
       tickCount++
       context.clearRect(0, 0, canvas.width, canvas.height)
       context.fillStyle = "#DDDDDD"
       context.fillRect(0,0,canvas.width,canvas.height)
       if (tickCount % 10 == 0) {
-        var oReq = new XMLHttpRequest()
+        let oReq = new XMLHttpRequest()
         oReq.onload = function() {
           sqlData = this.responseText.split(",")
         }
-        oReq.open("get", "get-data.php?id="+i, true)
+        oReq.open("get", "get-data.php", true)
         oReq.send()
       }
       if ((sqlData.length - 1) == cards.length) {
-        for (var i = 0; i < cards.length; i++) {
+        for (let i = 0; i < cards.length; i++) {
           desiredPos[i].x = sqlData[i].split("-")[0]
           desiredPos[i].y = sqlData[i].split("-")[1]
           cards[i].layer = sqlData[i].split("-")[2]
-          var x_velocity = Math.ceil(Math.abs(desiredPos[i].x - cards[i].x) / 30)
-          var y_velocity = Math.ceil(Math.abs(desiredPos[i].y - cards[i].y) / 30)
+          cards[i].id = parseInt(sqlData[i].split("-")[3]) - 1
+          let x_velocity = Math.ceil(Math.abs(desiredPos[i].x - cards[i].x) / 30)
+          let y_velocity = Math.ceil(Math.abs(desiredPos[i].y - cards[i].y) / 30)
           if (desiredPos[i].x > cards[i].x) cards[i].x += x_velocity
           if (desiredPos[i].x < cards[i].x) cards[i].x -= x_velocity
           if (desiredPos[i].y > cards[i].y) cards[i].y += y_velocity
           if (desiredPos[i].y < cards[i].y) cards[i].y -= y_velocity
         }
-        var sorted = getSortedCards()
-        for (var i = 0; i < sorted.length; i++) {
+        let s = getSortedCards()
+        for (let i = 0; i < cards.length; i++) {
           context.drawImage(
-            sorted[i].img,
-            sorted[i].x * (canvas.width / 1000),
-            sorted[i].y * (canvas.height / 1000),
-            sorted[i].img.width * size,
-            sorted[i].img.height * size
+            s[i].img,
+            s[i].x * (canvas.width / 1000),
+            s[i].y * (canvas.height / 1000),
+            s[i].img.width * size,
+            s[i].img.height * size
           )
         }
       }
     }
   }, 10)
 
+  function get_max_layer() {
+    let max_layer = 0
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].layer > max_layer)
+        max_layer = parseInt(cards[i].layer)
+    }
+    return max_layer
+  }
+
   function getSortedCards() {
-    var limit = cards.length
-    sortedCards = cards.slice()
-    while (limit > 0) {
-      for (var i = 0; i < limit - 1; i++) {
-        if (sortedCards[i].layer > sortedCards[i+1].layer) {
-          var temp = sortedCards[i]
+    let sortedCards = cards.slice()
+    for (let t = 0; t < cards.length-1; t++) {
+      for (let i = 0; i < cards.length-1; i++) {
+        if (parseInt(sortedCards[i].layer) > parseInt(sortedCards[i+1].layer)) {
+          let temp = sortedCards[i]
           sortedCards[i] = sortedCards[i+1]
           sortedCards[i+1] = temp
         }
       }
-      limit--
     }
-    max_layer = sortedCards[sortedCards.length-1].layer
     return sortedCards
   }
 
-  canvas.addEventListener("mousemove", function(e) {
-    cards.reverse()
-    for (var i = 0; i < cards.length; i++) {
-      mx = e.clientX
-      my = e.clientY
-      ex = Math.round(cards[i].x * (canvas.width / 1000))
-      ey = Math.round(cards[i].y * (canvas.height / 1000))
-      ew = cards[i].img.width * size
-      eh = cards[i].img.height * size
-      if (mx >= ex && mx <= ex+ew && my >= ey && my <= ey+eh && mouseState.down && !mouseState.dragging) {
+  function get_i(x,y) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].x == x && cards[i].y == y) {
+        return i
+      }
+    }
+  }
+
+  function check_card_collision(e) {
+    let s = getSortedCards()
+    let general_collison = false;
+    let rect = canvas.getBoundingClientRect();
+    for (let i = 0; i < cards.length; i++) {
+      let ri = (cards.length - 1) - i
+      mx = e.clientX - rect.left;
+      my = e.clientY - rect.top;
+      ex = s[ri].x * (canvas.width / 1000)
+      ey = s[ri].y * (canvas.height / 1000)
+      ew = s[ri].img.width * size
+      eh = s[ri].img.height * size
+      let collision = mx >= ex && mx <= ex+ew && my >= ey && my <= ey+eh
+      if (collision) {
+        general_collison = true
+      }
+      if (collision && mouseState.down && !mouseState.dragging) {
           relativePos.x = mx - ex
           relativePos.y = my - ey
           mouseState.dragging = true
-          mouseState.draggingId = (cards.length - 1) - i
-          break
+          mouseState.draggingId = get_i(s[ri].x, s[ri].y)
+          return general_collison
       }
     }
-    cards.reverse()
+    return general_collison
+  }
+
+  canvas.addEventListener("mousemove", function(e) {
+    let rect = canvas.getBoundingClientRect();
+    let general_collison = check_card_collision(e)
+    if (general_collison) {
+      canvas.style.cursor = "move"
+    } else {
+      canvas.style.cursor = "default"
+    }
     if (mouseState.dragging) {
       cards[mouseState.draggingId].x = Math.round((mx - relativePos.x) * (1000 / canvas.width))
       cards[mouseState.draggingId].y = Math.round((my - relativePos.y) * (1000 / canvas.height))
       context.clearRect(0, 0, canvas.width, canvas.height)
       context.fillStyle = "#DDDDDD"
       context.fillRect(0,0,canvas.width,canvas.height)
-      var sorted = getSortedCards()
-      for (var i = 0; i < sorted.length; i++) {
+      let s = getSortedCards()
+      for (let i = 0; i < cards.length; i++) {
         context.drawImage(
-          sorted[i].img,
-          sorted[i].x * (canvas.width / 1000),
-          sorted[i].y * (canvas.height / 1000),
-          sorted[i].img.width * size,
-          sorted[i].img.height * size
+          s[i].img,
+          s[i].x * (canvas.width / 1000),
+          s[i].y * (canvas.height / 1000),
+          s[i].img.width * size,
+          s[i].img.height * size
         )
       }
+      context.fillStyle = '#99FF99';
+      context.beginPath();
+      let i = mouseState.draggingId
+      let b = cards.slice()
+      let w = 5
+      context.moveTo(b[i].x * (canvas.width / 1000)-w, b[i].y * (canvas.height / 1000)-w);
+      context.lineTo(b[i].x * (canvas.width / 1000)+b[i].img.width * size+w,b[i].y * (canvas.height / 1000)-w);
+      context.lineTo(b[i].x * (canvas.width / 1000)+b[i].img.width * size+w, b[i].y * (canvas.height / 1000)+b[i].img.height * size+w);
+      context.lineTo(b[i].x * (canvas.width / 1000)-w, b[i].y * (canvas.height / 1000)+b[i].img.height * size+w);
+      context.closePath();
+      context.fill();
       context.drawImage(
         cards[mouseState.draggingId].img,
         cards[mouseState.draggingId].x * (canvas.width / 1000),
@@ -165,29 +211,32 @@
   canvas.addEventListener("mouseup", function(e) {
     mouseState.up = true
     mouseState.down = false
-    var x = Math.round((e.clientX - relativePos.x) * (1000 / canvas.width))
-    var y = Math.round((e.clientY - relativePos.y) * (1000 / canvas.height))
+    let rect = canvas.getBoundingClientRect();
+    mx = e.clientX - rect.left;
+    my = e.clientY - rect.top;
+    let x = Math.round((mx - relativePos.x) * (1000 / canvas.width))
+    let y = Math.round((my - relativePos.y) * (1000 / canvas.height))
     if (mouseState.dragging) {
-      var oReq = new XMLHttpRequest()
-      var new_layer = parseInt(max_layer) + 1
+      let oReq = new XMLHttpRequest()
+      let new_layer = get_max_layer() + 1
       oReq.open("get", "set-data.php?x=" + x + "&y=" + y + "&i=" + (mouseState.draggingId + 1) + "&l=" + new_layer, true)
       oReq.send()
       mouseState.dragging = false
+      console.log("MOVED #"+mouseState.draggingId,"TO",x,y)
+      cards[mouseState.draggingId].layer = new_layer
+      sqlData[mouseState.draggingId] = x + "-" + y + "-" + new_layer + "-" + (mouseState.draggingId+1)
     }
-    console.log("MOVED #"+mouseState.draggingId,"TO",x,y)
-    cards[mouseState.draggingId].layer = max_layer + 1
-    sqlData[mouseState.draggingId] = x + "-" + y
   }, false)
 
   function reset() {
-    var a_difference = 0;
-    var b_difference = 0;
-    var ab_step = 5;
-    for (var i = 0; i < cards.length; i++) {
-      var oReq = new XMLHttpRequest()
-      var x = Math.round(1000 / 2 - (cards[i].img.width * size) + a_difference - (ab_step * (cards.length / 2)))
-      var y = Math.round(1000 / 2 - (cards[i].img.height * size) + b_difference - (ab_step * (cards.length / 2)))
-      oReq.open("get", "set-data.php?x=" + x + "&y=" + y + "&i=" + (i+1) + "&l=0", true)
+    let a_difference = 0
+    let b_difference = 0
+    let ab_step = 5
+    for (let i = 0; i < cards.length; i++) {
+      let oReq = new XMLHttpRequest()
+      let x = Math.round(1000 / 2 - (cards[i].img.width * size) + a_difference - (ab_step * (cards.length / 2)))
+      let y = Math.round(1000 / 2 - (cards[i].img.height * size) + b_difference - (ab_step * (cards.length / 2)))
+      oReq.open("get", "set-data.php?x=" + x + "&y=" + y + "&i=" + (i+1) + "&l="+i, true)
       oReq.send()
       a_difference += ab_step
       b_difference += ab_step
@@ -199,10 +248,10 @@
   }
 
   function mix() {
-    for (var i = 0; i < cards.length; i++) {
-      var oReq = new XMLHttpRequest()
-      var x = Math.round(Math.random() * 600 + 200 - cards[i].img.width * size)
-      var y = Math.round(Math.random() * 600 + 200 - cards[i].img.height * size)
+    for (let i = 0; i < cards.length; i++) {
+      let oReq = new XMLHttpRequest()
+      let x = Math.round(Math.random() * 600 + 200 - cards[i].img.width * size)
+      let y = Math.round(Math.random() * 600 + 200 - cards[i].img.height * size)
       oReq.open("get", "set-data.php?x=" + x + "&y=" + y + "&i=" + (i+1) + "&l=0", true)
       oReq.send()
     }
